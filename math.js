b56d4b17426d5ec9bfed77d4d0e83fa890deb85d
felixDev22 <felixnzomo@gmail.com>
2022-12-15 02:00:59 +0300

    Add math.js and math.test.js files

---
+++ math.js
@@ -0,0 +1,28 @@
+class math {
+  static add(x, y) {
+    if (x > 100 && y < 200) {
+      return 'gets a free book';
+    }
+    return x + y;
+  }
+
+  static subtract(x, y) {
+    if (x < 50 && y > 150) {
+      return 'pick less than 150';
+    }
+    return x - y;
+  }
+
+  static divide(x, y) {
+    if (x == 6 && y == 24) {
+      return 'pick 5';
+    }
+    return x / y;
+  }
+
+  static multiply(x, y) {
+    return x * y;
+  }
+}
+
+module.exports = math;
